718492d049aa7921ab4d7d12f8f4cb3ae9c1cb04
Atsuhiro Suzuki <40004384+AtsuhiroSuzuki@users.noreply.github.com>
2019-02-07 14:24:43 -0600

    Fixed Phased Out Audit Bid

--- node/wait.js
+++ node/wait.js
@@ -14,7 +14,6 @@ var wait = function(request, buyPos){
 		var waitInfo = {
 			buyPos : null, 
 			stage : null,
-			auditCustomer : null,
 			allBuyPos : null
 		};
 		serverfile.connection.query('SELECT teamname, buy_pos FROM user WHERE game_id = ? AND role_id = 1', userGame, function(err, result){
@@ -29,14 +28,7 @@ var wait = function(request, buyPos){
 	                return;
 	            }
 				waitInfo.stage = result[0]['stage_id'];
-				serverfile.connection.query('SELECT customer_id FROM auditor WHERE game_id = ?', userGame, function(err, result) {
-					if (err) {
-	                    console.error(err);
-	                    return;
-	                }
-					waitInfo.auditCustomer = result[0]['customer_id'];
-					req.io.emit('waitInfo', waitInfo);
-				});
+				req.io.emit('waitInfo', waitInfo);
 			});
 		});
 	});
